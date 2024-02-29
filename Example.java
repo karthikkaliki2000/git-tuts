c1e70bda94269e2cf2926f7d8047894c76a317dc
karthikkaliki <karthikkaliki2000@gmail.com>
2024-02-29 14:50:29 +0530

    adding fun method

--- Example.java
+++ Example.java
@@ -1,5 +1,9 @@
 public class Example {
     public static void main(String[] args) {
         System.out.println("Java file");
+        fun();
+    }
+    public void fun(String name){
+       System.out.println("Hi "+name)
     }
 }
