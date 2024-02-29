e1b5632c9ed84382e9f53a252fb0ce8464975c19
karthikkaliki <karthikkaliki2000@gmail.com>
2024-02-29 15:00:09 +0530

    Major changes

--- Example.java
+++ Example.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Example {
     public static void main(String[] args) {
         System.out.println("Java file");
-        fun();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name");
+        fun(s.next());
     }
 
-    public void fun(String name){
-       System.out.println("Hi "+name);
+    public static void fun(String name) {
+        System.out.println("Hi " + name);
+        System.out.println("Welcome to this page " + name);
     }
 }
